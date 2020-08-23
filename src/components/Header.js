ee37a1381c25105ff456591f69ccb42e9fc59936
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-23 18:59:01 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,6 +14,7 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
+
   },
 }));
 
@@ -26,13 +25,9 @@ export default function Header() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+            <Typography variant="h6" className={classes.title}>
+                COVID-19 Tracker
+            </Typography>
         </Toolbar>
       </AppBar>
     </div>
