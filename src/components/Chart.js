e5b31faa4f3718de3bfe17972864cd401c2cd59c
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-26 15:56:01 +0500

    Update Chart.js

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,23 +1,46 @@
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Bar} from 'react-chartjs-2';
 
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      backgroundColor: 'rgba(255,99,132,0.2)',
-      borderColor: 'rgba(255,99,132,1)',
-      borderWidth: 1,
-      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-      hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
 
-function Chart() {
+
+function Chart({x}) {
+
+
+
+    const [fetchedChartData, SetfetchedChartData] = useState([{}]);
+
+    useEffect(() => {
+      async function fetchedChart() {
+        const Countryresponse = await fetch('https://api.thevirustracker.com/free-api?countryTotals=ALL');
+        const countriesData = await Countryresponse.json();
+        SetfetchedChartData(countriesData.countryitems[0])
+  
+      }
+      fetchedChart();
+    }, [SetfetchedChartData])
+  
+    const a = fetchedChartData && fetchedChartData[x] && fetchedChartData[x].title
+    const b = fetchedChartData && fetchedChartData[x] && fetchedChartData[x].total_cases
+    const c = fetchedChartData && fetchedChartData[x] && fetchedChartData[x].total_recovered
+    const d = fetchedChartData && fetchedChartData[x] && fetchedChartData[x].total_deaths
+
+    const data = {
+        labels: ['Total Cases', 'Total Recovered', 'Toral Deaths'],
+        datasets: [
+          {
+            label: a,
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: [b,c, d]
+          }
+        ]
+      };
+
+
     return (
         <div>
         <h2>Bar Example (custom size)</h2>
