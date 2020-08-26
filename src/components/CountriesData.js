f609ce1eb745d532bfc3e11ba58a62bcf41fda90
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-26 15:39:37 +0500

    Update CountriesData.js

--- src/components/CountriesData.js
+++ src/components/CountriesData.js
@@ -1,8 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,88 +18,97 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const useStyleTypography = makeStyles({
-    root: {
-      width: '50%',
-      maxWidth: 200,
-    },
-  });
+  root: {
+    width: '50%',
+    maxWidth: 200,
+  },
+});
 
-export default function CountriesData() {
+export default function CountriesData({ x }) {
+
   const classes = useStyles();
   const classTypography = useStyleTypography();
 
-  const [CountryData, SetCountryData] = useState();
- 
-  useEffect(()=> {
-      async function fetchCountrydata() {
-          
-          const CountryAPIresponse = await fetch('https://api.thevirustracker.com/free-api?countryTotals=ALL');
-          const countryDatafromAPI = await CountryAPIresponse.json();
-          SetCountryData(countryDatafromAPI.countryitems[0])
-        }
-          fetchCountrydata(); 
-  },[])
+  const [fetchedCountryData, SetfetchedCountryData] = useState([{}]);
 
-  const countryName = CountryData && CountryData[15].title
+  useEffect(() => {
+    async function fetchedCountryData() {
+      const Countryresponse = await fetch('https://api.thevirustracker.com/free-api?countryTotals=ALL');
+      const countriesData = await Countryresponse.json();
+      SetfetchedCountryData(countriesData.countryitems[0])
+
+    }
+    fetchedCountryData();
+  }, [SetfetchedCountryData])
+
+  const name = fetchedCountryData && fetchedCountryData[x] && fetchedCountryData[x].title
+  const cases = fetchedCountryData && fetchedCountryData[x] && fetchedCountryData[x].total_cases
+  const recovered = fetchedCountryData && fetchedCountryData[x] && fetchedCountryData[x].total_recovered
+  const deaths = fetchedCountryData && fetchedCountryData[x] && fetchedCountryData[x].total_deaths
+
+
   return (
     <div className={classes.root}>
-      <Grid container > 
-      <Grid item xs = {12} > 
-      <Paper elevation = {3}> 
-          <div className = {classTypography.root}> 
+      <Grid container >
+        <Grid item xs={12} >
+          <Paper elevation={3}>
+            <div className={classTypography.root}>
 
-                    <Typography variant="h5" gutterBottom style = {{color: 'black)', fontWeight: "bold"}}>
-                        {countryName}
-                    </Typography>
-          </div>
-        </Paper>
-      </Grid>
+              <Typography variant="h5" gutterBottom style={{ color: 'black)', fontWeight: "bold" }}>
+                {name}
+              </Typography>
+            </div>
+          </Paper>
+        </Grid>
       </Grid>
 
       <Grid container spacing={2} >
-        <Grid item xs = {4} > 
-        <Paper elevation = {3}> 
-          <div className = {classTypography.root}> 
-                    <Typography variant="h4" gutterBottom style = {{color: 'black', fontWeight: "bold"}}>
-                    
 
+        <Grid item xs={4} >
+          <Paper elevation={3}>
+            <div className={classTypography.root}>
+              <Typography variant="h4" gutterBottom style={{ color: 'black', fontWeight: "bold" }}>
+                                {cases}
+              </Typography>
+              <Typography variant="subtitle2" gutterBottom style={{ color: 'black)', fontWeight: "bold" }}>
+                Total Cases
                     </Typography>
-                    <Typography variant="subtitle2" gutterBottom style = {{color: 'black)', fontWeight: "bold"}}>
-                           Total Cases
-                    </Typography>
-          </div>
-        </Paper>
-          </Grid>
-        <Grid item xs = {4} > 
-        <Paper elevation = {3}> 
-          <div className = {classTypography.root}> 
-                    <Typography variant="h4" gutterBottom style = {{color: 'black', fontWeight: "bold"}}>
-                                 
+            </div>
+          </Paper>
+        </Grid>
 
+        <Grid item xs={4} >
+          <Paper elevation={3}>
+            <div className={classTypography.root}>
+              <Typography variant="h4" gutterBottom style={{ color: 'black', fontWeight: "bold" }}>
+                            {recovered}
+
+              </Typography>
+              <Typography variant="subtitle2" gutterBottom style={{ color: 'black)', fontWeight: "bold" }}>
+                Total Recoveries
                     </Typography>
-                    <Typography variant="subtitle2" gutterBottom style = {{color: 'black)', fontWeight: "bold"}}>
-                    Recovered
-                    </Typography>
-          </div>
-        </Paper>
-         </Grid>
-        <Grid item xs = {4} > 
-        <Paper elevation = {3}> 
-          <div className = {classTypography.root}> 
-                    <Typography variant="h4" gutterBottom style = {{color: 'black', fontWeight: "bold"}}>
-                                
-                    </Typography>
-                    <Typography variant="subtitle2" gutterBottom style = {{color: 'black)', fontWeight: "bold"}}>
-                    Deaths
-                    </Typography>
-          </div>
-        </Paper>
+            </div>
+          </Paper>
+        </Grid>
+        <Grid item xs={4} >
+          <Paper elevation={3}>
+            <div className={classTypography.root}>
+              <Typography variant="h4" gutterBottom style={{ color: 'black', fontWeight: "bold" }}>
+                         {deaths}
+
+              </Typography>
+              <Typography variant="subtitle2" gutterBottom style={{ color: 'black)', fontWeight: "bold" }}>
+                Total Deaths
+              </Typography>
+
+            </div>
+          </Paper>
         </Grid>
 
 
       </Grid>
 
-      </div>
+    </div>
   );
 }
 
