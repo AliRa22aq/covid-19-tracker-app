bddb070a2521ad6f1bb81371c139a5893618b74e
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-27 22:33:12 +0500

    Update ContryPicker.js

--- src/components/ContryPicker.js
+++ src/components/ContryPicker.js
@@ -10,7 +10,7 @@ const useStyles2 = makeStyles((theme) => ({
   }));
 
 
-export default function ContryPicker({ handleCountryChange }) {
+export default function ContryPicker({handleCountryChange}) {
 
     const classes = useStyles2();
 
@@ -21,21 +21,21 @@ export default function ContryPicker({ handleCountryChange }) {
             const Countryresponse = await fetch('https://api.thevirustracker.com/free-api?countryTotals=ALL');
             const countriesData = await Countryresponse.json();
             SetfetchedCountry(countriesData.countryitems[0])
+            
         }
         fetchCountry();
     }, [])
-
+    
     return (
         <div>
-            <FormControl >
-                <NativeSelect defaultValue="Select" onChange={(e) => handleCountryChange(e.target.value)} >
+            <FormControl className = {classes.root}>
+                <NativeSelect defaultValue="Select" onChange = {(e) => handleCountryChange(e.target.value)}>
                     <option value={0}>  Global  </option>
                     {Object.keys(fetchedCountry).map((val, i) => {
                         return (
-                            <option key={i} value={val}>  {fetchedCountry[val].title}  </option>
+                            <option key={i} value={val} >  {fetchedCountry[val].title}  </option>
                         )
                     })}
-
                 </NativeSelect>
             </FormControl>
         </div>
