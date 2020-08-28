5d78f87aac67360c5eb245ee91d62052ba73d35d
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-28 17:41:48 +0500

    Update CountriesData2.js

--- src/components/CountriesData2.js
+++ src/components/CountriesData2.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import CountUp from 'react-countup';
-import { Grid } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import Loading from './Loading'
 
