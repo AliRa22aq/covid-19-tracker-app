05ac69a31c6de00abf8c0d42fc99971fac7c8f2d
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-28 15:14:38 +0500

    Update GlobalChart.js

--- src/components/GlobalChart.js
+++ src/components/GlobalChart.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {Pie} from 'react-chartjs-2';
+import {Line} from 'react-chartjs-2';
 
 
 
@@ -22,6 +23,31 @@ function GlobalChart() {
   const total_deaths = globalData && globalData.total_deaths
 
   
+  const [dailyData, SetDailyData] = useState({})
+  useEffect(() => {
+ 
+    async function Dailychart() {
+      const APIdata = await fetch('https://covid19.mathdro.id/api/daily');
+      const pretty = await APIdata.json();
+      //SetDailyData(pretty.timelineitems[0])
+      SetDailyData(pretty)
+     }
+     Dailychart();
+   }, [])
+ 
+ const LabelsforLine = Object.values(dailyData).map((v)=> {
+   return ( v.reportDate )
+ })
+ const ConfirmedforLine = Object.values(dailyData).map((v)=> {
+   return ( v.confirmed.total )
+ })
+ const DeathsforLine = Object.values(dailyData).map((v)=> {
+   return ( v.deaths.total )
+ })
+
+
+ 
+// Pi chart
   const data = {
     labels: [
       'Deaths',
@@ -43,11 +69,64 @@ function GlobalChart() {
     }]
   };
   
-  
+   // LIne CHart
+
+const data2 = {
+  labels: LabelsforLine,
+  datasets: [
+    {
+      label: 'Confirmed',
+      data: ConfirmedforLine,
+      fill: true,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      
+    },
+    {
+      label: 'Deaths',
+      data: DeathsforLine,
+      fill: true,
+      lineTension: 0.1,
+      backgroundColor: 'red',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'red',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'red',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      
+    }
+  ]
+};
+
   return (
     <div>
-    <h1 style= {{textAlign: "center"}}> Global </h1>
-    <Pie data={data} />
+   <h1 style= {{textAlign: "center"}}> Global </h1>
+        <Line data={data2} />
+ 
+
   </div>
   )
 }
